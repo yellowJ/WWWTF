4bc0014525201e49da4d63428860b99b75f31d7f
yellowJ <122454124@qq.com>
2016-09-09 16:58:39 +0800

    test commit

--- app/src/main/java/com/hasoji/poke/MainActivity.java
+++ app/src/main/java/com/hasoji/poke/MainActivity.java
@@ -3,11 +3,15 @@ package com.hasoji.poke;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.pokegoapi.api.PokemonGo;
+import com.pokegoapi.api.pokemon.Pokemon;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // test
     }
 }
